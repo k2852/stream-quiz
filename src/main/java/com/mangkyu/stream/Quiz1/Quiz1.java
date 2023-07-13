1fe9624e267e498008456c77a5d29abe7d6f17f7
soonseok2 <kwon2852@gmail.com>
2023-07-13 20:21:22 +0900

    quiz1 description added

--- src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
+++ src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -16,9 +16,9 @@ public class Quiz1 {
         List<String[]> csvLines = readCsvLines();
 
         return csvLines.stream()
-                .map(line -> line[1].replaceAll("\\s", ""))
-                .flatMap(hobbies -> Arrays.stream(hobbies.split(":")))
-                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, (oldValue, newValue) -> newValue += oldValue));
+                .map(line -> line[1].replaceAll("\\s", "")) // line[] 배열의 두번째 인덱스인 취미란에서 정규식으로 공백 제거
+                .flatMap(hobbies -> Arrays.stream(hobbies.split(":"))) // 공백이 제거된 취미를 flatMap으로 잘라서 Stream으로
+                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, (oldValue, newValue) -> newValue += oldValue)); // 각 취미별로 빈도를 센다
 
     }
 
@@ -27,10 +27,10 @@ public class Quiz1 {
         List<String[]> csvLines = readCsvLines();
 
         return csvLines.stream()
-                .filter(line -> line[0].startsWith("정"))
-                .map(line -> line[1].replaceAll("\\s", ""))
-                .flatMap(hobbies -> Arrays.stream(hobbies.split(":")))
-                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, (oldValue, newValue) -> newValue += oldValue));
+                .filter(line -> line[0].startsWith("정")) // line[] 배열의 첫번째 인덱스인 이름에서 정씨로 필터링
+                .map(line -> line[1].replaceAll("\\s", "")) // 동일하게 취미에서 공백 제거
+                .flatMap(hobbies -> Arrays.stream(hobbies.split(":"))) // 취미들만 flatMap으로 모음
+                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, (oldValue, newValue) -> newValue += oldValue)); // 각 취미 빈도수
     }
 
     // 1.3 소개 내용에 '좋아'가 몇번 등장하는지 계산하여라.
@@ -38,8 +38,8 @@ public class Quiz1 {
         List<String[]> csvLines = readCsvLines();
 
         return csvLines.stream()
-                .map(line -> countContains(line[2], 0))
-                .reduce(0, Integer::sum);
+                .map(line -> countContains(line[2], 0)) // 소개 내용에서 '좋아' 등장 횟수 카운트
+                .reduce(0, Integer::sum); // 각 String에서의 카운트의 총합을 계산한 후 반환(Identity)
     }
 
     private static final String TARGET = "좋아";
